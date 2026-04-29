f8518b69b28b8d9c852a5e1a37cb3184353e9bad
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-04-29 02:49:26 +0000

    Apr 29, 2026, 2:49 AM

--- t03_maths.js
+++ t03_maths.js
@@ -8,15 +8,15 @@
  let age = "15";
  let money = "50";
  let year = "2026";
- let birthyear = "2010";
- let old age = "2026";
+ let birthYear = "2010";
+ let oldAge = "age + 10";
  /*********************************
  
   main code
 
 *********************************/
 console.log("Hi "+ username + " as of " + year +" you are " + age + " years old. You have " + money + " dollars. ")
-
+console.log("you were born in" + "birth year" )
 /*********************************
  
   functions
