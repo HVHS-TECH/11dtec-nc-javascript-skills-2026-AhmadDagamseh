a5123bd163bf5678a90309807ae0f47592ca72a2
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-04-29 02:32:37 +0000

    Apr 29, 2026, 2:32 AM

--- t02_variables.js
+++ t02_variables.js
@@ -4,15 +4,15 @@
   console.log( "Running T1-intro.js");
   console.log( "hello world");
  //variables
- let username = "Ahmad"
- let age = "15"
- let money = "50"
+ let username = "Ahmad";
+ let age = "15";
+ let money = "50";
  /*********************************
  
   main code
 
 *********************************/
-console.log("hi"+ username + "as of" + year +"you are" + age + "years old. You have" + money + "dollars");
+console.log("hi"+ username + "as of" + year +"you are" + age + "years old. You have " + money + "dollars");
 
 /*********************************
  
