1541dba9a33dfb3f73a7065607f07ee1c895aa42
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-04-29 02:43:33 +0000

    Apr 29, 2026, 2:43 AM

---
+++ t03_maths.js
@@ -0,0 +1,21 @@
+/* Paste/*********************************
+  name of task:
+*********************************/
+  console.log( "Running T1-intro.js");
+  console.log( "hello world");
+ //variables
+ varnum1
+ num2 
+ answer
+ /*********************************
+ 
+  main code
+
+*********************************/
+console.log("Hi "+ username + " as of " + year +" you are " + age + " years old. You have " + money + " dollars. ")
+
+/*********************************
+ 
+  functions
+
+*********************************/
