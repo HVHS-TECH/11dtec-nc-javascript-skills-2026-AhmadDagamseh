da44ddef69c27dc73387195cbe48d035bb019fa9
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-04-29 02:24:47 +0000

    Apr 29, 2026, 2:24 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,7 @@
-/* Paste your code from task 1 here */
+/* Paste/*********************************
+  name of task:
+*********************************/
+  console.log( "Running T1-intro.js");
+  console.log( "hello world");
+ //variables
+ your code from task 1 here */
