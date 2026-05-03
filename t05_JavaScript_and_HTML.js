4d49a48b6cc2fd3ba7a9ea76ade78e74c24e4676
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-05-03 22:29:48 +0000

    May 3, 2026, 10:29 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,3 +1,29 @@
-<div id="javascriptoutput">
-<p>This will be replaced</p>
-</div>
+/* Paste/*********************************
+  name of task:
+*********************************/
+  console.log( "Running T1-intro.js");
+  console.log( "hello world");
+ //variables
+ let username = "Ahmad";
+ let age = "15";
+ let money = "20";
+ let year = "2026";
+ let birthYear = "2010";
+ let oldAge = "25";
+ let extraMoney = "13"
+ /*********************************
+ 
+  main code
+
+*********************************/
+console.log("Hi  "+ username + " as of " + year +" you are " + age + " years old. You have " + money + " dollars. ")
+console.log("You were born in " + birthYear )
+console.log("In ten years you will be " + oldAge )
+console.log("You have " + money + " dollars " )
+console.log("You spend half your money, you now have " + money/2 + " dollars" )
+console.log("You get 3 dollars, now you have " + extraMoney + " dollars " )
+/*********************************
+ 
+  functions
+
+*********************************/
