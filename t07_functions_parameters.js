e2f62333246c157c7c2c23f0115ba97cb0764858
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-05-04 23:58:28 +0000

    May 4, 2026, 11:58 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,37 @@
-/* Paste your code from task 6 here */
+/* Paste/*********************************
+  name of task:
+*********************************/
+  console.log("T07 - Functions With Parameters");
+
+ //variables
+ let username = "Ahmad";
+ let age = "15";
+ let money = "20";
+ let year = "2026";
+ let birthYear = "2010";
+ let oldAge = "25";
+ let extraMoney = "13"
+ /*********************************
+ 
+  main code
+
+*********************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+OUTPUT.innerHTML = "<p>Hi " + username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars.</p>";
+OUTPUT.innerHTML += "<p>You were born in " + birthYear + "</p>";
+OUTPUT.innerHTML += "<p>In ten years you will be " + oldAge + "</p>";
+OUTPUT.innerHTML += "<p>You have " + money + " dollars</p>";
+OUTPUT.innerHTML += "<p>You spend half your money, you now have " + money/2 + " dollars</p>";
+OUTPUT.innerHTML += "<p>You get 3 dollars, now you have " + extraMoney + " dollars</p>";
+
+/*********************************
+ 
+  functions
+
+*********************************/
+function welcome() {
+    OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+}
+welcome();
