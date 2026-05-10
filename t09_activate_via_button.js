7ba6778023665d5631001ca2ca215ac22a090d38
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-05-10 22:17:52 +0000

    May 10, 2026, 10:17 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,16 @@
-/* Paste your code from task 7 here*/
+console.log("Running Task 09...");
+
+const output = document.getElementById("spaceForJavaScriptOutput");
+
+/****************************
+ Main Code
+****************************/
+
+/****************************
+ Functions
+****************************/
+function start() {
+  console.log("Button pressed!");
+
+  output.innerHTML = "<p>You pressed the button!</p>";
+}
