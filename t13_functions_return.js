fe670f486c0093c4f23381e68e2d5e099bcf0496
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-05-13 03:08:53 +0000

    May 13, 2026, 3:08 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,31 @@
-/* Paste your code from the last task */
+// T13 - Functions with return values
+// Ahmad D. - HVHS
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const FORM = document.getElementById("userForm");
+const AGE_FIELD = document.getElementById("ageField");
+const MONEY_FIELD = document.getElementById("moneyField");
+
+// Function that RETURNS change
+function calculateChange(_money, _price) {
+  return _money - _price;
+}
+
+FORM.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let age = Number(AGE_FIELD.value);
+  let money = Number(MONEY_FIELD.value);
+
+  const PRICE = 4;
+
+  OUTPUT.innerHTML = `<p>A chocolate bar costs $${PRICE}</p>`;
+
+  if (money >= PRICE) {
+    let change = calculateChange(money, PRICE);
+    OUTPUT.innerHTML += `<p>You CAN afford a chocolate bar</p>`;
+    OUTPUT.innerHTML += `<p>You will get $${change} change</p>`;
+  } else {
+    OUTPUT.innerHTML += `<p>Sorry you CAN'T afford a chocolate bar</p>`;
+  }
+});
