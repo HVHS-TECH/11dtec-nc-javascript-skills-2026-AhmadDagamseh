c950f2ea06f9da75376d01a7c5e2af8e96f59668
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-05-13 02:33:08 +0000

    May 13, 2026, 2:33 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,28 @@
-/* Paste your code from the last task */
+// T11 - Data Types
+// Ahmad D. - HVHS
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const FORM = document.getElementById("userForm");
+const AGE_FIELD = document.getElementById("ageField");
+const MONEY_FIELD = document.getElementById("moneyField");
+
+FORM.addEventListener("submit", function(event) {
+  event.preventDefault(); // stop page refresh
+
+  // get values from form (strings)
+  let ageString = AGE_FIELD.value;
+  let moneyString = MONEY_FIELD.value;
+
+  // convert to numbers
+  let ageNumber = Number(ageString);
+  let moneyNumber = Number(moneyString);
+
+  // update HTML output
+  OUTPUT.innerHTML = `
+    <p>Your age: ${ageNumber}</p>
+    <p>Your pocket money: $${moneyNumber}</p>
+
+    <p>Age type: ${typeof(ageNumber)}</p>
+    <p>Money type: ${typeof(moneyNumber)}</p>
+  `;
+});
