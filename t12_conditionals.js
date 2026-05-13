7030325e248e58afa46f4c41bc248ff5bcffbee0
AhmadDagamseh <24090ad@hvhs.school.nz>
2026-05-13 02:43:02 +0000

    May 13, 2026, 2:43 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,33 @@
-/* Paste your code from the last task */
+// T12 - Conditionals
+// Ahmad D. - HVHS
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const FORM = document.getElementById("userForm");
+const AGE_FIELD = document.getElementById("ageField");
+const MONEY_FIELD = document.getElementById("moneyField");
+
+FORM.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // get values (strings)
+  let ageString = AGE_FIELD.value;
+  let moneyString = MONEY_FIELD.value;
+
+  // convert to numbers
+  let age = Number(ageString);
+  let money = Number(moneyString);
+
+  // chocolate bar price
+  const PRICE = 4;
+
+  // build output
+  let message = `<p>A chocolate bar costs $${PRICE}</p>`;
+
+  if (money >= PRICE) {
+    message += `<p>You CAN afford a chocolate bar</p>`;
+  } else {
+    message += `<p>Sorry you CAN'T afford a chocolate bar</p>`;
+  }
+
+  OUTPUT.innerHTML = message;
+});
